b1f1626a1b5baaf46172af2b1c43eafbdc5e704b
darkchii <darkchii@qq.com>
2026-02-06 22:53:07 +0800

    LiquidationBot Project for binance.

--- monitor_binance.py
+++ monitor_binance.py
@@ -38,9 +38,9 @@ class MonitorBinance:
                         # 减少日志量
                         if abs(fluctuation) > get_price_volatility_threshold(current_price):
                             self.Log.info(f"💴 代币: {data['s']} | 价格: {data['p']} | 价格涨跌: {fluctuation * 100:.4f}%")
-                            # TODO: 去触发批量扫描，发现在这个价格波动下的高风险用户钱包，但还没想好如何处理这些钱包地址，
-                            #  因为重点要等到预言机上的价格和该价格相等时，才能去进行清算处理，
-                            #  或许我可以标记这些钱包地址以及价格，然后另外再写一个模块，监听预言机价格变动
+                            # TODO: 触发批量扫描，扫描在这个价格波动下的高风险用户钱包，
+                            #  等到预言机上的价格和该价格相等时，进行清算处理，
+                            #  这里可以只标记这些钱包地址以及价格，然后另外再写一个模块监听预言机价格变动
 
                         await self._db.update_binance_price(f"binance_price", vtoken_addr, current_price)
 
