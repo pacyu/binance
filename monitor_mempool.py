c84b5bac7203c53d712fa60e08743505b1944938
darkchii <darkchii@qq.com>
2026-02-12 19:12:44 +0800

    LiquidationBot Project for binance.

--- monitor_mempool.py
+++ monitor_mempool.py
@@ -113,7 +113,7 @@ class MonitorMemPool:
                     prices = parse_prices(report)
                     final_price = sorted(prices)[len(prices) // 2]
                     decimals = int(digest_config['decimals'])
-                    price = final_price * (10 ** (18 - decimals)) # 将价格放缩为18位精度（wei)
+                    price = final_price * (10 ** (36 - decimals))
 
                     symbol = digest_config['symbol']
                     vtoken_address = digest_config['v_address']
