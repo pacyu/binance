64732b240a6e266fd3f91c5b0dea7da4717c4888
darkchii <darkchii@qq.com>
2026-02-13 16:08:53 +0800

    LiquidationBot Project for binance.

--- monitor_users.py
+++ monitor_users.py
@@ -55,7 +55,7 @@ class MonitorUsers:
 
         prices = await self._client.get_oracle_price(list(self._vtoken_cache.keys()))
 
-        batch_size = 80
+        batch_size = 1500
 
         tasks = [
             self._process_users(user_address_list[i: i + batch_size], prices)
